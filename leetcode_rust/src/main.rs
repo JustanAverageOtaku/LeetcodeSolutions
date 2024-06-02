863eee5c0852999abdeaa971425a695e33f234c8
Ibrahi Shah <naegi100@gmail.com>
2024-06-03 02:09:50 +0500

    Find minimum arrow shots

--- leetcode_rust/src/main.rs
+++ leetcode_rust/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use overlapping_intervals::erase_overlap_intervals;
+use overlapping_intervals::find_min_arrow_shots;
 
 mod daily_temperatures;
 mod overlapping_intervals;
@@ -10,12 +10,12 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
     let v:Vec<Vec<i32>> = vec![
-        vec![1,100],
-        vec![11,22],
-        vec![1,11],
-        vec![2,12]
+        vec![1,2],
+        vec![3,4],
+        vec![5,6],
+        vec![7,8]
     ];
-    let s = format!("{:?}", erase_overlap_intervals(v));
+    let s = format!("{:?}", find_min_arrow_shots(v));
 
     println!("{}", s);
     println!("Hello, world!");
--- leetcode_rust/src/overlapping_intervals.rs
+++ leetcode_rust/src/overlapping_intervals.rs
@@ -18,3 +18,19 @@ pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
 
     count
 }
+
+#[allow(dead_code)]
+pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
+    points.sort_by(|a, b| a[1].cmp(&b[1]));
+
+    let mut count = 1;
+    let mut min:i32 = points[0][1];
+    for i in 1..points.len() {
+        if points[i][0] > min {
+            min = points[i][1];
+            count += 1;
+        }
+    }
+
+    count
+}
